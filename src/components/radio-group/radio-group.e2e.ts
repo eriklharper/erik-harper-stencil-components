ef554dffc7090801466884f6281f6d783f1ca8e0
Erik Harper-Dicianno <eri11048@eharper.home>
2020-04-08 14:47:32 -0700

    Working e2e test for prop passing

--- src/components/radio-group/radio-group.e2e.ts
+++ src/components/radio-group/radio-group.e2e.ts
@@ -8,4 +8,30 @@ describe('erik-radio-group', () => {
     const element = await page.find('erik-radio-group');
     expect(element).toHaveClass('hydrated');
   });
+  it('defaults to small', async () => {
+    const page = await newE2EPage();
+    await page.setContent('<erik-radio-group></erik-radio-group>');
+
+    const element = await page.find('erik-radio-group');
+    expect(element.getAttribute("scale")).toBe("small");
+  });
+  it('radio-buttons receive necessary props', async () => {
+    const page = await newE2EPage();
+    await page.setContent(`
+      <erik-radio-group name="radio">
+        <erik-radio-button value="one" checked>
+              One
+        </erik-radio-button>
+        <erik-radio-button value="two">
+            Two
+        </erik-radio-button>
+      </erik-radio-group>
+    `);
+
+    const radio = await page.find('erik-radio-button');
+    const name = await radio.getProperty("name");
+    const scale = await radio.getProperty("scale");
+    expect(name).toBe("radio");
+    expect(scale).toBe("small");
+  });
 });
